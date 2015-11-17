e815e3b89b9e40488b6d41016cfb5710fe4dd4cd
Phuong <phuongnhu0894@gmail.com>
2015-11-17 10:06:45 +0700

    no message

--- UITableView/ViewController.m
+++ UITableView/ViewController.m
@@ -10,6 +10,7 @@
 #import "Model.h"
 
 @interface ViewController () <UITableViewDataSource, UITableViewDelegate>
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 
 
@@ -26,15 +27,24 @@
     //[self.tableView registerClass:[UITableViewCell class ]forCellReuseIdentifier:@"Cell"];
     models = [[NSMutableArray alloc]init];
     Model *model1 = [Model new];
-    model1.name = @"Elephant";
+    model1.name = @"Ngoc Trinh";
     model1.measure = @"100-100-100";
-    model1.imageFile = @"elephant.png";
+    model1.imageFile = @"ngoctrinh.png";
     [models addObject:model1];
-    model1 = [Model new];
-    model1.name = @"Tiger";
-    model1.measure = @"100-100-100";
-    model1.imageFile = @"tiger.png";
-    [models addObject:model1];
+    Model *model2 = [Model new];
+    model2 = [Model new];
+    model2.name = @"Taylor";
+    model2.measure = @"100-100-100";
+    model2.imageFile = @"taylor.png";
+    [models addObject:model2];
+    Model *model3 = [Model new];
+    model3.name = @"Rebecca";
+    model3.measure = @"100-100-100";
+    model3.imageFile = @"rebecca.png";
+    [models addObject:model3];
+     self.tableView.separatorColor = [UIColor redColor];
+    
+    
 }
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
     return 1;
